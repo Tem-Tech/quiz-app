1c80b629897f618d8b1f4ad72ee754c73c145335
Tem-Tech <oyeyemileanne@gmail.com>
2022-03-30 11:20:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Questions from "./pages/Questions";
+import Settings from "./pages/Settings";
+import FinalScreen from "./pages/FinalScreen";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+      <Settings />
+      <Questions />
+      <FinalScreen />
     </div>
   );
 }
---
+++ src/pages/Questions.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Data from "./Data";
+import ReactDOM from "react-dom";
+// store the questions
+
+// display score if all questions are completed using ternary operator condition ? expressionIfTrue : expressionIfFalse;
+const Questions = () => {
+  return <div>Questions</div>;
+};
+
+export default Questions;
